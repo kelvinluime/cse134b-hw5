f859ff625eb453b6c22d6eee89706ac9ecde55e0
kelvinluime <kelflo1120@gmail.com>
2019-12-10 14:44:46 -0800

    main page frontend wip

--- apiClient.js
+++ apiClient.js
@@ -4,8 +4,23 @@ const createXHR = () => {
     try { return new ActiveXObject("Msxm12.XMLHTTP.3.0"); } catch (e) {}
     try { return new ActiveXObject("Msxm12.XMLHTTP"); } catch (e) {}
     try { return new ActiveXObject("Microsoft.XMLHTTP"); } catch (e) {}
-    alert("XMLHttpRequest not supported");
     return null;
+}
+
+const handler = (xhr, resolve, reject) => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        resolve(xhr);
+    } else if (xhr.readyState == 4 && xhr.status != 200) {
+        reject(xhr);
+    }
+}
+
+const logoutHandler = (xhr, resolve, reject) => {
+    if (xhr.readyState == 4 && xhr.status == 204) {
+        resolve(xhr);
+    } else if (xhr.readyState == 4 && xhr.status != 204) {
+        reject(xhr);
+    }
 }
 
 /**
@@ -15,49 +30,117 @@ const createXHR = () => {
  * @param {string} password 
  * @param {*} handler 
  */
-const signUp = (username, email, password, handler) => {
-    const url = "http://fa19server.appspot.com/api/users";
-    const data = {
-        username,
-        email,
-        password
-    };
-    const params = Object.keys(data).map((k) => {
-        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-    }).join('&');
-    const xhr = createXHR();
-    if (xhr) {
-        xhr.open("POST", url);
-        xhr.onreadstatechange = () => handler(xhr);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.send(params);
-    }
+const signUp = (data) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/users";
+        const params = Object.keys(data).map((k) => {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
+        }).join('&');
+        const xhr = createXHR();
+        if (xhr) {
+            xhr.open("POST", url);
+            xhr.onreadystatechange = () => handler(xhr, resolve, reject);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send(params);
+        } else {
+            alert("XMLHttpRequest not supported");
+            reject(null);
+        }
+    })
 }
 
-const login = () => {
-
+const login = (data) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/users/login";
+        const params = Object.keys(data).map((k) => {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        }).join('&');
+        const xhr = createXHR();
+        if (xhr) {
+            xhr.open("POST", url);
+            xhr.onreadystatechange = () => handler(xhr, resolve, reject);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send(params);
+        }
+    })
 }
 
-const logout = () => {
-
+const logout = (accessToken) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/users/logout?access_token=" + accessToken;
+        const xhr = createXHR();
+        if (xhr) {
+            xhr.open("POST", url);
+            xhr.onreadystatechange = () => logoutHandler(xhr, resolve, reject);
+            xhr.send(null);
+        }
+    })
 }
 
-const retrieveAllMovies = () => {
-
+const retrieveAllItems = (accessToken) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/wishlists/myWishlist?access_token=" + accessToken;
+        const xhr = createXHR();
+        if (xhr) {
+            xhr.open("GET", url);
+            xhr.onreadystatechange = () => handler(xhr, resolve, reject);
+            xhr.send(null);
+        }
+    })
 }
 
-const addNewMovie = () => {
-
+const addNewItem = (accessToken, data) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/wishlists?access_token=" + accessToken;
+        const params = Object.keys(data).map((k) => {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        }).join('&');
+        const xhr = createXHR();
+        if (xhr) {
+            xhr.open("POST", url);
+            xhr.onreadystatechange = () => handler(xhr, resolve, reject);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send(params);
+        }
+    })
 }
 
-const getMoviesByMovieId = () => {
-
+const getItemByItemId = (accessToken, itemId) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/wishlists/" + itemId + "?access_token=" + accessToken;
+        const xhr = createXHR();
+        if (xhr) {
+            xhr.open("GET", url);
+            xhr.onreadystatechange = () => handler(xhr, resolve, reject);
+            xhr.send(null);
+        }
+    })
 }
 
-const deleteTargetMovie = () => {
-
+const deleteTargetItem = (accessToken, itemId) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/wishlists/" + itemId + "?access_token=" + accessToken;
+        const xhr = createXHR();
+        if (xhr) {
+            xhr.open("DELETE", url);
+            xhr.onreadystatechange = () => handler(xhr, resolve, reject);
+            xhr.send(null);
+        }
+    })
 }
 
-const updateMovie = () => {
-
+const updateItem = (accessToken, itemId, data) => {
+    return new Promise((resolve, reject) => {
+        const url = "http://fa19server.appspot.com/api/wishlists/" + itemId + "/replace?access_token=" + accessToken;
+        const xhr = createXHR();
+        const params = Object.keys(data).map((k) => {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+        }).join('&');
+        if (xhr) {
+            xhr.open("POST", url);
+            xhr.onreadystatechange = () => handler(xhr, resolve, reject);
+            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            xhr.send(params);
+        }
+    })
 }
---
+++ index.js
@@ -0,0 +1,4 @@
+handleClickInfoDialogCancel = () => {
+    document.querySelector('#info-dialog').style.display = 'none';
+    document.querySelector('#grey-out').style.display = 'none';
+}
